2924dff6715f73de16d8d6b6b013b23b56f41c64
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-06-18 18:40:10 -0300

    Final Version

--- src/test/java/edu/austral/ingsis/FileSystemTests.java
+++ src/test/java/edu/austral/ingsis/FileSystemTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 public class FileSystemTests {
@@ -12,9 +13,10 @@ public class FileSystemTests {
   private final FileSystemRunner runner = new FileSystemRunnerImpl();
 
   private void executeTest(List<Map.Entry<String, String>> commandsAndResults) {
-    final List<String> commands = commandsAndResults.stream().map(Map.Entry::getKey).toList();
+    final List<String> commands =
+        commandsAndResults.stream().map(Map.Entry::getKey).collect(Collectors.toList());
     final List<String> expectedResult =
-        commandsAndResults.stream().map(Map.Entry::getValue).toList();
+        commandsAndResults.stream().map(Map.Entry::getValue).collect(Collectors.toList());
 
     final List<String> actualResult = runner.executeCommands(commands);
 
